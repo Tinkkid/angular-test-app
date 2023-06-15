cb6988274e7bc784e000eab461b56b6d1d1a6c89
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-06-15 15:57:40 +0200

    Created car selectors

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css'],
+})
+export class CarComponent implements OnInit {
+  model: string;
+  speed: number;
+  name: string;
+  colors: Colors;
+  options: string[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.name = 'Audi';
+    this.speed = 210;
+    this.model = 'RS8';
+    this.colors = {
+      car: 'White',
+      salon: 'Black',
+      wheels: 'Silver',
+    };
+    this.options = ['ABS', 'ESP', 'Cruise Control'];
+  }
+
+  carSelect(carName: string) {
+    if (carName === 'bmw') {
+      this.name = 'BMW';
+      this.speed = 200;
+      this.model = 'M5';
+      this.colors = {
+        car: 'Black',
+        salon: 'Black',
+        wheels: 'White',
+      };
+      this.options = ['ABS', 'ESP', 'Cruise Control'];
+    } else if (carName === 'audi') {
+      this.name = 'Audi';
+      this.speed = 210;
+      this.model = 'RS8';
+      this.colors = {
+        car: 'White',
+        salon: 'Black',
+        wheels: 'Silver',
+      };
+      this.options = ['ABS', 'ESP', 'Cruise Control'];
+    } else {
+      this.name = 'Mercedes';
+      this.speed = 180;
+      this.model = 'C180';
+      this.colors = {
+        car: 'White',
+        salon: 'Black',
+        wheels: 'Silver',
+      };
+      this.options = ['ABS', 'ESP'];
+    }
+  }
+}
+
+interface Colors {
+  car: string;
+  salon: string;
+  wheels: string;
+}
