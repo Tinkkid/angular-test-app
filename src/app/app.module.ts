7541df77deef469584da9564f145f8fd50b6d0ce
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-06-16 07:50:55 +0200

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
+const appRoutes: Routes = [
+  { path: '', component: CarComponent },
+  { path: 'about', component: ContactsComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, CarComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, CarComponent, ContactsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
